d3b94ff00a871d73b40166b1eb5ea039c70f72af
primaananda <emailiputuprimaananda@gmail.com>
2018-03-29 15:17:23 +0700

    add more

--- main_pdfminer.py
+++ main_pdfminer.py
@@ -1,3 +1,45 @@
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter#process_pdf
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+
+#from cStringIO import StringIO
+from io import BytesIO
+
+def pdf_to_text(pdfname):
+
+    # PDFMiner boilerplate
+    rsrcmgr = PDFResourceManager()
+    sio = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, sio, codec=codec, laparams=laparams)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+    # Extract text
+    fp = file(pdfname, 'rb')
+    for page in PDFPage.get_pages(fp):
+        interpreter.process_page(page)
+    fp.close()
+
+    # Get text from StringIO
+    text = sio.getvalue()
+
+    # Cleanup
+    device.close()
+    sio.close()
+
+    return text
+
+def main():
+    text = pdf_to_text('resources/tesaurus2008.pdf')
+    file = open('hasil/tesaurus_pdfminer.txt','w')
+    file.write(text)
+    file.close()
+    
+main()
+
+"""
 from cStringIO import StringIO
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
@@ -16,6 +58,7 @@ def convert(fname, pages=None):
     interpreter = PDFPageInterpreter(manager, converter)
     
     infile = file(fname, 'rb')
+    
     for page in PDFPage.get_pages(infile, pagenums):
         interpreter.process_page(page)
     infile.close()
@@ -26,6 +69,7 @@ def convert(fname, pages=None):
 
 
 text = convert('resources/tesaurus2008.pdf',)
-file = open('hasil_with_pdfminer.txt','w')
+file = open('hasil/tesaurus_pdfminer.txt','w')
 file.write(text)
 file.close()
+"""
