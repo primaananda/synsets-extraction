44ab7799803b1b0308dc6c4a632a57cc9747355e
primaananda <emailiputuprimaananda@gmail.com>
2018-04-10 23:24:28 +0700

    update clear text 50%

--- main_cleartext.py
+++ main_cleartext.py
@@ -2,36 +2,46 @@
 import re
 file = open('hasil/final/finaltesaurus.txt','r')
 files = open('hasil/final/tesaurus.txt','w')
+
 lines = file.readline()
 
 dicts = []
 dicts2 = []
 
-
 while lines:
     if(not lines.startswith('\t')):
-        #sentences = lines.replace('\n','')
-        dicts.append(lines)
-        lines = file.readline()
+        dicts.append(lines.rstrip())
+    lines = file.readline()
     if not lines:
         break
+
+letter = '###'
+for x in range(0,len(dicts)):
+    temp = ''
+    if(dicts[x].startswith('#')):
+        letter = dicts[x][1].lower()
+    if(dicts[x].startswith(letter)):
+        temp += dicts[x]
+        while(dicts[x].endswith(',')) or (dicts[x].endswith('-')):
+            temp += dicts[x+1]
+            x =  x+1
+        dicts2.append(temp)
+
 '''
+
 for x in range(0,len(dicts)):
     test = ''
     print(dicts[x])
-    if (dicts[x].startswith('#')) or (dicts[x].endswith(',\n')) or (dicts[x].endswith('-\n')):
+    if (dicts[x].startswith('#')) or (dicts[x].endswith(',')) or (dicts[x].endswith('-')):
         temp = ''
-        while dicts[x].endswith(',\n') or dicts[x].endswith('-\n') :
+        while dicts[x].endswith(',') or dicts[x].endswith('-') :
             temp += dicts[x]
             x = x+1
         dicts2.append(temp)
-        
-for x in dicts2:
-    files.write(x)
 '''
 
-for ln in dicts:
-    file.write(ln):
-
+for x in dicts2:
+    files.write(x+'\n')
+    
 file.close()
 files.close()
