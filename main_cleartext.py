a24a800ee2031bb445787e2d515539cbba3d6805
primaananda <emailiputuprimaananda@gmail.com>
2018-04-10 18:25:00 +0700

    update 10 april

--- main_cleartext.py
+++ main_cleartext.py
@@ -1,8 +1,37 @@
 #created by primaananda
-file = open('hasil/finaltesaurus.txt','r')
-
+import re
+file = open('hasil/final/finaltesaurus.txt','r')
+files = open('hasil/final/tesaurus.txt','w')
 lines = file.readline()
+
+dicts = []
+dicts2 = []
+
+
 while lines:
-    print lines
-    lines = file.readline()
+    if(not lines.startswith('\t')):
+        #sentences = lines.replace('\n','')
+        dicts.append(lines)
+        lines = file.readline()
+    if not lines:
+        break
+'''
+for x in range(0,len(dicts)):
+    test = ''
+    print(dicts[x])
+    if (dicts[x].startswith('#')) or (dicts[x].endswith(',\n')) or (dicts[x].endswith('-\n')):
+        temp = ''
+        while dicts[x].endswith(',\n') or dicts[x].endswith('-\n') :
+            temp += dicts[x]
+            x = x+1
+        dicts2.append(temp)
+        
+for x in dicts2:
+    files.write(x)
+'''
+
+for ln in dicts:
+    file.write(ln):
+
 file.close()
+files.close()
