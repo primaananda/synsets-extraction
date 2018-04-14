ba8413c406548fe18bb0916317e53b41013aa880
primaananda <emailiputuprimaananda@gmail.com>
2018-04-14 21:10:00 +0700

    new coding file

--- main.py
+++ main.py
@@ -2,11 +2,17 @@
 
 import csv
 
-
+def txt_to_csv(file, files):
+    dicts = []
+    for line in file:
+        dicts = line.split()
+        #writer = csv.write(files)
+        print dicts
 
 def main():
     file_text = open('hasil/final/tesaurus_clear_text.txt','r')
     file_csv = open('hasil/final/tesaurus.csv','w')
     
+    hasil = txt_to_csv(file_text, file_csv)
     
 main()
