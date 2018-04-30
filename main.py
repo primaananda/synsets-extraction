f7fbca7253e7bb391312081551968051f9e2b717
primaananda <emailiputuprimaananda@gmail.com>
2018-04-30 18:44:03 +0700

    new PY YEAA

--- main.py
+++ main.py
@@ -4,30 +4,29 @@ import pandas as pd
 from collections import defaultdict
 
 def cari_pasangan(file_csv):
-    '''
-    columns = defaultdict(list) # each value in each column is appended to a list
-    reader = csv.DictReader(file_csv)
-    for row in reader:
-        for (k,v) in row.items():
-            columns[k].append(v)
-    #print columns['Noun']
-    #print columns['Verb']
-    #print columns['Adjektiva']
-    #print columns['Adverb']
-    '''
     noun = []
     verb = []
     adje = []
     adve = []
-    reader = csv.reader(file_csv)
+    pasangan = []
+    reader = csv.reader(file_csv, delimiter=',')
     for row in reader:
         noun.append(row[1])
         verb.append(row[2])
         adje.append(row[3])
         adve.append(row[4])
-    for x in noun:
+        pasangan.append(row[1]+row[2]+row[3]+row[4])
+        #pasangan.append(row[2])
+        #pasangan.append(row[3])
+        #pasangan.append(row[4])
+        #pasangan.append([row[1],row[2],row[3],row[4]])
+    for x in pasangan:
         print x
+    #    line = x.split(',')
+    #print line
+    return pasangan
 
+#def ekstraksi_synset_indonesia():
 
 def main():
     open_csv = open('hasil/final/tesaurus.csv','r')
