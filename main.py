1a9312315a1c8a8f7e9b7f5b2b0e53758ea431fc
primaananda <emailiputuprimaananda@gmail.com>
2018-06-02 21:39:28 +0700

    aa

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ def main():
     for x in data:
         print synsets_gen(x)
 
-main()
+#main()
 
-print(synsets_gen("ahad"))
+#print(synsets_gen("ahad"))
+
+aa =  ['one', 'two', 'three']
+bb = ['two', 'one', 'three']
+def testcase(a, b):
+    print a == b
+    return a == b
+
+testcase(aa,bb)
