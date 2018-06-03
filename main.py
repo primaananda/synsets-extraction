688c6cc56dbec9b4b96db0e5abe6fcf959ff91eb
primaananda <emailiputuprimaananda@gmail.com>
2018-06-03 17:18:59 +0700

    add

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ def main():
 
 #print(synsets_gen("ahad"))
 
-aa =  ['one', 'two', 'three']
-bb = ['two', 'one', 'three']
+aa =  ['one', 'two', 'three', 'satu']
+bb = ['two', 'one', 'three', 'kygo', 'anton']
+
 def testcase(a, b):
-    print a == b
+    print a <= b
     return a == b
 
 testcase(aa,bb)
