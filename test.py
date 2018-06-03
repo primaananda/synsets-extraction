1b0c64d051c5c0ce6fcf1e52570b797bb215f2c0
primaananda <emailiputuprimaananda@gmail.com>
2018-06-03 17:18:59 +0700

    add

--- test.py
+++ test.py
@@ -9,17 +9,17 @@ class SyncSetTest(unittest.TestCase):
     # def test_sample(self):
     #     input = synsets_gen('ahad', open('datatest/1.json'))
     #     output = [['ahad', 'minggu'], ['ahad', 'esa', 'satu', 'tunggal']]
-    #     self.assertEqual(input, output, 'first test case pass')
+    #     self.assertEqual(input, output, '\nresult: {}\nexpected: {}'.format(input, output))
     #
     # def test_sample2(self):
     #     input = synsets_gen('setanggi', open('datatest/2.json'))
     #     output = [[]]
-    #     self.assertEqual(input, output, '2nd test case pass')
+    #     self.assertEqual(input, output, '\nresult: {}\nexpected: {}'.format(input, output))
     #
     # def test_sample3(self):
     #     input = synsets_gen('aborsi', open('datatest/3.json'))
     #     output = [['aborsi', 'pengguguran']]
-    #     self.assertEqual(input, output, '3rd test case pass')
+    #     self.assertEqual(input, output, '\nresult: {}\nexpected: {}'.format(input, output))
     #
     # def test_sample4(self):
     #     input = synsets_gen('pekan', open('datatest/4.json'))
