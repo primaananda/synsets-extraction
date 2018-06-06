fbeb39af2c848f80ea13aded5eda427e1cd43503
primaananda <emailiputuprimaananda@gmail.com>
2018-06-07 00:37:37 +0700

    delete zero list and redundant data

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
+import json
 from alt_gen import alt_gen
-from synsets import synsets_gen
 
-# def main():
-#     for x in data:
-#         print (synsets_gen(x))
-#
-# #main()
-#
-# #print(synsets_gen("ahad"))
-#
-# aa =  ['one', 'two', 'three', 'satu']
-# bb = ['two', 'one', 'three', 'kygo', 'anton']
-#
-# def testcase(a, b):
-#     print a <= b
-#     return a == b
-#
-# testcase(aa,bb)
+#open file
+files = open('datatest/1.json')
 
-input = alt_gen('ahad', open('datatest/1.json'))
-print(input)
+def del_redundant(synsets):
+    new_synset = []
+    for synset in synsets:
+        #print(len(synset))
+        #remove zero synset in list
+        if len(synset) != 0:
+            new_synset.append(synset)
+        #end remove zero synset
+    #remove duplicate synsets
+    new_set = set(tuple(syns) for syns in new_synset)
+    new_synset = [ list(syns) for syns in new_set]
+    #end remove duplicate synsets
+    print(new_synset)
+    return new_synset
+
+def synsets_extraction(file):
+    thesa = json.load(file)
+    calon_synsets = []
+    for word in thesa:
+        output = alt_gen(word, open('datatest/1.json'))
+        for synsets in output:
+            calon_synsets.append(synsets)
+    file.close()
+    return calon_synsets
+
+#input = alt_gen('perdamaian', file)
+dat = synsets_extraction(files)
+del_redundant(dat)
